8054ab942bf826cee062b1d993479b67dec4f0df
Bhavna <bhavna.rani199@gmail.com>
2025-01-04 11:12:19 +0530

    Delete testing directory

--- testing/provider.tf
+++
@@ -1,4 +0,0 @@
-# provider.tf
-provider "aws" {
-  region = "us-east-1"  # Update with your preferred AWS region
-}
--- testing/route_table_association.tf
+++
@@ -1,5 +0,0 @@
-# route_table_association.tf
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
