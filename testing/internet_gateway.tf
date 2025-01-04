29d394f9a480e54026dd9adc0841650cd634d056
Bhavna <bhavna.rani199@gmail.com>
2025-01-04 10:39:02 +0530

    Delete testing directory

--- testing/internet_gateway.tf
+++
@@ -1,9 +0,0 @@
-# internet_gateway.tf
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name    = var.igw_tag
-    Service = "Terraform"
-  }
-}
--- testing/s3.tf
+++
@@ -1,9 +0,0 @@
-# s3.tf
-resource "aws_s3_bucket" "bhavnabucket231" {
-  bucket = "bhavnabucket231"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
